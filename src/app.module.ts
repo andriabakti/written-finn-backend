dcdad5702e06a4736a16622f62bddd146f00a3bd
Andria Bakti <andr.bkti@gmail.com>
2023-01-06 16:51:03 +0700

    Initialize connection to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -10,8 +11,33 @@ import { ConfigModule } from '@nestjs/config';
       ignoreEnvFile: true,
       isGlobal: true,
     }),
+    MongooseModule.forRootAsync({
+      useFactory: async () => {
+        const uri =
+          process.env.NODE_ENV == 'development'
+            ? `mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_NAME}?directConnection=true`
+            : `mongodb://${process.env.MONGODB_USER}:${encodeURIComponent(
+                process.env.MONGODB_PASS,
+              )}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${
+                process.env.MONGODB_NAME
+              }`;
+        console.log('uri >', uri);
+        return {
+          uri: uri,
+          useCreateIndex: true,
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useFindAndModify: false,
+        };
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  // constuctor() { };
+  async onApplicationBootstrap() {
+    //
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import compression from 'compression';
+import helmet from 'helmet';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const port = process.env.PORT || 3939;
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  app.use(helmet());
+  app.use(compression());
+  app.setGlobalPrefix('/api/v1');
   await app.listen(port);
 }
 bootstrap();
